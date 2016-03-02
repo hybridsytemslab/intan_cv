34a6a8286ea9e4b8fe5e625a44c41aa8b62e1059
Max Jackson <mjacksoncfa@gmail.com>
2016-03-02 14:33:45 -0500

    Mockup

---
+++ spikeplot_cv2.cpp
@@ -0,0 +1,6 @@
+#include "spikeplot_cv2.h"
+
+spikeplot_cv2::spikeplot_cv2()
+{
+
+}
---
+++ spikeplot_cv2.h
@@ -0,0 +1,11 @@
+#ifndef SPIKEPLOT_CV2_H
+#define SPIKEPLOT_CV2_H
+
+
+class spikeplot_cv2
+{
+public:
+    spikeplot_cv2();
+};
+
+#endif // SPIKEPLOT_CV2_H
